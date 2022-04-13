fc60ec57221a2fc43dcb90b4a911e9371a8b0f05
Kollecollier <kristoffer.collier@live.se>
2022-04-13 09:17:36 +0000

    Add style to css

--- assets/css/JS/script.js
+++
@@ -1,9 +0,0 @@
-/// Doms and possible choices
-
-const buttons = document.getElementsByClassName("control");
-const playerScore = document.getElementById("player-score");
-const computerScore = document.getElementById("computer-score");
-const messages = document.getElementById("messages");
-const choices = ["1", "2", "3", "4", "5", "6"];
-
-//** Add event listners to all buttons */ */
