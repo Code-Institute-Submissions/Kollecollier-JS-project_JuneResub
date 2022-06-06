a8ec21b7443a06699fa74c8eba3b0bbb897ad7f4
Kollecollier <kristoffer.collier@live.se>
2022-06-06 09:36:31 +0000

    Add code for score result on html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,24 +2,27 @@ const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
 const choiceBtns = document.querySelectorAll(".choiceBtn");
+const playerScoreDiv = document.querySelector("#playerScore");
+const computerScoreDiv = document.querySelector("#computerScore");
 let player;
 let computer;
-let result;
+let playerScore = 0, compScore = 0;
 
 choiceBtns.forEach(button => button.addEventListener("click", () => {
-
   player = button.textContent;
-  computerTurn ();
+  computerTurn();
   playerText.textContent = `Player: ${player}`;
   computerText.textContent = `Computer: ${computer}`;
   resultText.textContent = checkWinner();
+  playerScoreDiv.textContent = playerScore;
+  computerScoreDiv.textContent = compScore;
+
 }));
 
-
-function computerTurn(){
+function computerTurn() {
   const randNum = Math.floor(Math.random() * 3) + 1;
 
-  switch(randNum){
+  switch (randNum) {
     case 1:
       computer = "✊";
       break;
@@ -32,20 +35,32 @@ function computerTurn(){
   }
 }
 
-
-
-function checkWinner(){
-  if(player == computer){
-      score = score + 1;
+function checkWinner() {
+  if (player === computer) {
     return "Draw!";
-  }
-  else if(computer == "✊"){
-    return (player == "✋") ? "You Win!" : "You Lose!"
-  }
-  else if(computer == "✋"){
-    return (player == "✌️") ? "You Win!" : "You Lose!"
-  }
-  else if(computer == "✌️"){
-    return (player == "✊") ? "You Win!" : "You Lose!"
+  } else if (computer === "✊") {
+    if (player === "✋") {
+      playerScore += 1;
+      return "You Win!";
+     } else {
+      compScore += 1;
+      return "You Lose!";
+     }
+  } else if (computer === "✋") {
+    if (player === "✌️") {
+      playerScore += 1;
+      return "You Win!";
+     } else {
+      compScore += 1;
+      return "You Lose!";
+     }
+  } else if (computer === "✌️") {
+    if (player === "✊") {
+      playerScore += 1;
+      return "You Win!";
+     } else {
+      compScore += 1;
+      return "You Lose!";
+     }
   }
 }
